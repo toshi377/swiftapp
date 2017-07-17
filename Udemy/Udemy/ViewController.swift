09e3e6df62b87380347447145b5291ed89636be8
荒井俊洋 <araitoshihiro@TA-MacBook-Air.local>
2017-07-17 21:40:30 +0900

    String interpolation

---
+++ Udemy/Udemy/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Udemy
+//
+//  Created by 荒井俊洋 on 2017/07/17.
+//  Copyright © 2017年 荒井俊洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func theButton(_ sender: Any) {
+        print(text1.text!)
+        print(text2.text!)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
